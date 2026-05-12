96461b6b2e065d418e97a7283f2e70198b03b2b8
quriosity <131264405+Quriosity0@users.noreply.github.com>
2026-05-12 23:45:02 +0300

    fixing issues

--- modules/tg-ws-proxy.nix
+++ modules/tg-ws-proxy.nix
@@ -1,6 +1,11 @@
 { inputs, pkgs, ... }:
 let
   tg-ws-proxy-pkg = inputs.tg-ws-proxy.packages.${pkgs.stdenv.hostPlatform.system}.default;
+  notify-script = pkgs.writeShellScript "tg-ws-proxy-notify" ''
+    sleep 2
+    link=$(journalctl --user -u tg-ws-proxy -n 30 --no-pager | grep "tg://" | sed "s/.*tg:/tg:/")
+    ${pkgs.libnotify}/bin/notify-send -t 0 "tg-ws-proxy" "$link"
+  '';
 in
 {
   home.packages = [ tg-ws-proxy-pkg ];
@@ -12,7 +17,8 @@ in
     };
 
     Service = {
-      ExecStartPost = "${pkgs.bash}/bin/bash -c 'sleep 2 && ${pkgs.libnotify}/bin/notify-send -t 0 \"tg-ws-proxy\" \"$(journalctl --user -u tg-ws-proxy -n 30 --no-pager | grep \"tg://\" | sed \"s/.*tg:/tg:/\")'";
+      ExecStart = "${tg-ws-proxy-pkg}/bin/tg-ws-proxy --port 1080 --dc-ip 2:149.154.167.220 --dc-ip 4:149.154.167.220";
+      ExecStartPost = "${notify-script}";
       Restart = "on-failure";
       RestartSec = "5s";
     };
