87bbc25b3606ac5ad6cdb0c625f6a7d596d6fd69
quriosity <131264405+Quriosity0@users.noreply.github.com>
2026-05-12 23:43:21 +0300

    added link output as notiflication

--- modules/tg-ws-proxy.nix
+++ modules/tg-ws-proxy.nix
@@ -12,7 +12,7 @@ in
     };
 
     Service = {
-      ExecStart = "${tg-ws-proxy-pkg}/bin/tg-ws-proxy --port 1080 --dc-ip 2:149.154.167.220 --dc-ip 4:149.154.167.220";
+      ExecStartPost = "${pkgs.bash}/bin/bash -c 'sleep 2 && ${pkgs.libnotify}/bin/notify-send -t 0 \"tg-ws-proxy\" \"$(journalctl --user -u tg-ws-proxy -n 30 --no-pager | grep \"tg://\" | sed \"s/.*tg:/tg:/\")'";
       Restart = "on-failure";
       RestartSec = "5s";
     };
