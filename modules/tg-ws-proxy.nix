6f397f2b28f4a574ca81d55860b48c4130e92dcb
quriosity <131264405+Quriosity0@users.noreply.github.com>
2026-05-13 00:06:16 +0300

    adding bugs

--- modules/tg-ws-proxy.nix
+++ modules/tg-ws-proxy.nix
@@ -3,24 +3,25 @@ let
   tg-ws-proxy-pkg = inputs.tg-ws-proxy.packages.${pkgs.stdenv.hostPlatform.system}.default;
 
   connect-script = pkgs.writeShellApplication {
-    name = "tg-ws-proxy-connect";
-    runtimeInputs = [ pkgs.xdg-utils pkgs.systemd pkgs.procps ];
-    text = ''
-      # Ждём пока Telegram запустится (до 5 минут)
-      for i in $(seq 1 60); do
-        if pgrep -x "telegram-desktop" > /dev/null; then
-          sleep 2
-          link=$(journalctl --user -u tg-ws-proxy -n 30 --no-pager \
-            | grep "tg://" \
-            | sed "s/.*tg:/tg:/")
-          if [ -n "$link" ]; then
-            xdg-open "$link"
-          fi
-          exit 0
+  name = "tg-ws-proxy-connect";
+  runtimeInputs = [ pkgs.xdg-utils pkgs.systemd pkgs.procps ];
+  text = ''
+    attempts=0
+    while [ "$attempts" -lt 60 ]; do
+      if pgrep -x "telegram-desktop" > /dev/null; then
+        sleep 2
+        link=$(journalctl --user -u tg-ws-proxy -n 30 --no-pager \
+          | grep "tg://" \
+          | sed "s/.*tg:/tg:/")
+        if [ -n "$link" ]; then
+          xdg-open "$link"
         fi
-        sleep 5
-      done
-    '';
+        exit 0
+      fi
+      attempts=$((attempts + 1))
+      sleep 5
+    done
+  '';
   };
 in
 {
