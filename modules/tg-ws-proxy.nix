88fd637cffceff583301d1f1c47d5dac7d6bc632
quriosity <131264405+Quriosity0@users.noreply.github.com>
2026-05-12 23:56:30 +0300

    trying to make it a bit automatic

--- modules/tg-ws-proxy.nix
+++ modules/tg-ws-proxy.nix
@@ -1,10 +1,16 @@
 { inputs, pkgs, ... }:
 let
   tg-ws-proxy-pkg = inputs.tg-ws-proxy.packages.${pkgs.stdenv.hostPlatform.system}.default;
-  notify-script = pkgs.writeShellScript "tg-ws-proxy-notify" ''
+  notify-and-connect-script = pkgs.writeShellScript "tg-ws-proxy-notify" ''
     sleep 2
-    link=$(journalctl --user -u tg-ws-proxy -n 30 --no-pager | grep "tg://" | sed "s/.*tg:/tg:/")
-    ${pkgs.libnotify}/bin/notify-send -t 0 "tg-ws-proxy" "$link"
+    link=$(journalctl --user -u tg-ws-proxy -n 30 --no-pager \
+      | grep "tg://" \
+      | sed "s/.*tg:/tg:/")
+
+    if [ -n "$link" ]; then
+      ${pkgs.libnotify}/bin/notify-send -t 0 "tg-ws-proxy" "$link"
+      ${pkgs.xdg-utils}/bin/xdg-open "$link"
+    fi
   '';
 in
 {
@@ -13,14 +19,17 @@ in
   systemd.user.services.tg-ws-proxy = {
     Unit = {
       Description = "Telegram WS Proxy";
-      After = [ "network.target" ];
+      After = [ "network.target" "graphical-session.target" ];
+      Wants = [ "graphical-session.target" ];
     };
 
     Service = {
       ExecStart = "${tg-ws-proxy-pkg}/bin/tg-ws-proxy --port 1080 --dc-ip 2:149.154.167.220 --dc-ip 4:149.154.167.220";
-      ExecStartPost = "${notify-script}";
+      ExecStartPost = "${notify-and-connect-script}";
       Restart = "on-failure";
       RestartSec = "5s";
+      # Пробрасываем переменные Wayland-сессии
+      Environment = "PATH=${pkgs.xdg-utils}/bin:${pkgs.libnotify}/bin";
     };
 
     Install = {
