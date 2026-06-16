3c5f67279153f5e3117fd9bad6c7d9dc97230ec7
quriosity <131264405+Quriosity0@users.noreply.github.com>
2026-06-17 02:07:56 +0300

    fixed zapret warnings in console while rebuilding

--- modules/zapret.nix
+++ modules/zapret.nix
@@ -2,7 +2,7 @@
 {
   services.zapret-discord-youtube = {
     enable = true;
-    config = "general(ALT)";  # choosing config
+    configName = "general(ALT)";  # choosing config
     gameFilter = "null";       # "null", "all", "tcp" or "udp"
     listGeneral = [ "rutracker.org" "github.com" "steambrew.app" "itch.io" "steamcommunity.com" "steampowered.com" ];  # additional domains
   };
