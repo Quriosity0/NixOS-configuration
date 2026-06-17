e752a93ebd310a652752fb355789142439da1e1d
quriosity <131264405+Quriosity0@users.noreply.github.com>
2026-06-17 03:01:48 +0300

    still fixing this shit

--- modules/zapret.nix
+++ modules/zapret.nix
@@ -5,6 +5,6 @@
     configName = "general(ALT)";  # choosing config
     gameFilter = "null";       # "null", "all", "tcp" or "udp"
     listGeneral = [ "rutracker.org" "github.com" "steambrew.app" "itch.io" "steamcommunity.com" "steampowered.com" ];  # additional domains
-    listExclude = [ "microsoft365.com" "office.com" "cloud.microsoft" ]; # exclude these adresses
+    listExclude = [ "microsoft365.com" "office.com" "cloud.microsoft" "microsoft.com" "office.net" ]; # exclude these adresses
   };
 }
