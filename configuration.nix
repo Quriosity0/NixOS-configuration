80ef8e012f974391ae7c64d9da1f1d0f632e15f8
quriosity <131264405+Quriosity0@users.noreply.github.com>
2026-08-20 03:56:35 +0300

    refactor: looks like I don't need it

--- configuration.nix
+++ configuration.nix
@@ -4,7 +4,6 @@
   imports =
     [
       ./hardware-configuration.nix
-      <home-manager/nixos>
       # ...
     ];
 
@@ -302,101 +301,4 @@
 
   nixpkgs.config.allowUnfree = true;
   system.stateVersion = "25.11";
-
-  # --------------------------------------------------------------------------------------------------------
-  # ------------------------------------- HOME MANAGER CONFIGURATION ---------------------------------------
-  # --------------------------------------------------------------------------------------------------------
-
-  home-manager.users.quriosity = { pkgs, ... }: {
-    home = {
-      stateVersion = "25.11";
-      packages = with pkgs; [
-        (prismlauncher.override {
-          additionalPrograms = [ ffmpeg ];
-          jdks = [
-            temurin-bin-8
-            temurin-bin-17
-            temurin-bin-25
-          ];
-        })
-        droidcam
-        sourcegit
-        faugus-launcher
-        vmware-workstation
-        materialgram
-        hardinfo2
-        vlc
-        zed-editor
-        xnviewmp
-        distrobox
-        distrobox-tui
-        boxbuddy
-
-        nur.repos.trev.helium
-        nur.repos.quriosity.zen-browser
-        nur.repos.quriosity.BedrockNix
-
-        transmission_4-gtk
-        qt6Packages.qtstyleplugin-kvantum
-        kdePackages.kdenlive
-
-        gnome-tweaks
-        refine
-        gnomeExtensions.blur-my-shell
-        gnomeExtensions.dash2dock-lite
-        gnomeExtensions.just-perfection
-        gnomeExtensions.clipboard-indicator
-        gnomeExtensions.app-hider
-        gnomeExtensions.rounded-window-corners-reborn
-        gnomeExtensions.caffeine
-        gnomeExtensions.tiling-shell
-        gnomeExtensions.status-tray
-        gnomeExtensions.user-themes
-      ];
-      username = "quriosity";
-      homeDirectory = "/home/quriosity";
-    };
-
-    programs = {
-      obs-studio = {
-        enable = true;
-        package = (
-          pkgs.obs-studio.override {
-            cudaSupport = true;
-          }
-        );
-        plugins = with pkgs.obs-studio-plugins; [
-          obs-backgroundremoval
-          obs-pipewire-audio-capture
-          obs-vaapi
-          obs-gstreamer
-          obs-vkcapture
-          input-overlay
-          obs-multi-rtmp
-          obs-livesplit-one
-        ];
-      };
-    };
-
-    dconf = {
-      enable = true;
-      settings = {
-        "org/gnome/shell" = {
-          enabled-extensions = [
-            pkgs.gnomeExtensions.blur-my-shell.extensionUuid
-            pkgs.gnomeExtensions.dash2dock-lite.extensionUuid
-            pkgs.gnomeExtensions.just-perfection.extensionUuid
-            pkgs.gnomeExtensions.clipboard-indicator.extensionUuid
-            pkgs.gnomeExtensions.app-hider.extensionUuid
-            pkgs.gnomeExtensions.rounded-window-corners-reborn.extensionUuid
-            pkgs.gnomeExtensions.caffeine.extensionUuid
-            pkgs.gnomeExtensions.tiling-shell.extensionUuid
-            pkgs.gnomeExtensions.status-tray.extensionUuid
-            pkgs.gnomeExtensions.user-themes.extensionUuid
-          ];
-        };
-      };
-    };
-    nixpkgs.config.allowUnfree = true;
-  };
 }
