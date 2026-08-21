763f7322765871d722e544c1cdb51046ba66b758
quriosity <131264405+Quriosity0@users.noreply.github.com>
2026-08-21 08:26:06 +0300

    refactor (spicetify): add all snipets, extensions and change theme

--- modules/spicetify.nix
+++ modules/spicetify.nix
@@ -11,17 +11,45 @@ in
   programs.spicetify = {
     enable = true;
 
-    theme = spicePkgs.themes.fluent;
+    theme = spicePkgs.themes.bloom;
     colorScheme = "dark";
 
     enabledExtensions = with spicePkgs.extensions; [
       fullAppDisplay
       shuffle
+      adblock
+      volumePercentage
+      beautifulLyrics
+      sidebarCustomizer
     ];
 
     enabledSnippets = with spicePkgs.snippets; [
       rotatingCoverart
       pointer
+      modernScrollbar
+      betterLyricsStyle
+      removeRecentlyPlayed
+      removePopular
+      hideLikedSongsCard
+      removeConnectBar
+      autoHideFriends
+      alwaysShowForward
+      hideRecentSearches
+      fullscreenHidePlayingFrom
+      hideNowPlayingViewButton
+      centeredLyrics
+      smoothProgressBar
+      hideDownloadButton
+      removeTopSpacing
+      hideFriendActivityButton
+      hideWhatsNewButton
+      smallerRightSidebarCover
+      hidePodcastButton
+      hideAudiobooksButton
+      hideMiniPlayerButton
+      fixMainViewWidth
+      hideSidebarScrollbar
+      leftAlignedHeartIcons
     ];
 
     enabledCustomApps = with spicePkgs.apps; [
