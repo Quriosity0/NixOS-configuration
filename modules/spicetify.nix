0e893147eb125590ea1b41697da65237e742f12f
quriosity <131264405+Quriosity0@users.noreply.github.com>
2026-08-26 00:38:24 +0300

    feat: add wavelink spicetify app

--- modules/spicetify.nix
+++ modules/spicetify.nix
@@ -55,6 +55,15 @@ in
     enabledCustomApps = with spicePkgs.apps; [
       marketplace
       lyricsPlus
+      ({
+        src = pkgs.fetchFromGitHub {
+          owner = "xoft122";
+          repo = "wavelink";
+          rev = "a7476eeda090e33299ca5c7aa0f1a78f35402bef";
+          hash = "sha256-uazgXeKymEMwWR7JZBPbTQHGAbHMVpQhtsPRgFsWxKw=";
+        };
+        name = "Wavelink";
+        })
     ];
   };
 }
